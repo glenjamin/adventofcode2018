e0547bf454fc23485272dace870303e773155a3e
Glen Mailer <glen@stainlessed.co.uk>
2018-12-12 23:27:52 +0000

    Rust day1 part 2

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -1,11 +1,39 @@
+use std::collections::HashSet;
 use std::io::{self, BufRead};
 
 fn main() {
-    let n = io::stdin()
+    let numbers = load_numbers();
+
+    let total: i32 = numbers.iter().sum();
+
+    let repeat = calculate_repeat(&numbers);
+
+    println!("Total: {}", total);
+    println!("Repeat: {:?}", repeat);
+}
+
+fn calculate_repeat(numbers: &[i32]) -> i32 {
+    let mut frequency = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+
+    for n in numbers.iter().cycle() {
+        frequency += n;
+
+        if seen.contains(&frequency) {
+            return frequency;
+        }
+
+        seen.insert(frequency);
+    }
+
+    unreachable!();
+}
+
+fn load_numbers() -> Vec<i32> {
+    io::stdin()
         .lock()
         .lines()
         .filter_map(|res| res.ok())
-        .filter_map(|s| s.trim().parse().ok())
-        .fold(0, |acc, x: i32| acc + x);
-    println!("Total: {}", n);
+        .filter_map(|s| s.trim().parse::<i32>().ok())
+        .collect()
 }
