0f9a68a48e1c137de5682e8680466b1a02e9d511
Glen Mailer <glen@stainlessed.co.uk>
2018-12-14 17:02:16 +0000

    Working day2

--- day2/rust/src/main.rs
+++ day2/rust/src/main.rs
@@ -1,19 +1,91 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    let stdin_lock = stdin.lock();
-    let ids = load_ids(stdin_lock);
+    let ids: Vec<_> = load_ids(io::stdin().lock()).collect();
 
-    println!("Checksum: {:?}", ids.collect::<Vec<String>>());
+    println!("Checksum: {:?}", checksum(ids.iter().map(|s| &s[..])));
+    println!("ID: {:?}", common_id(&ids));
 }
 
 fn load_ids(stream: impl BufRead) -> impl Iterator<Item = String> {
-    stream
-        .lines()
-        .flatten()
-        .map(|s| s.trim_end().to_string())
-        .filter(|s| !s.is_empty())
+    stream.lines().flatten().filter(|s| startswith_alpha(s))
+}
+
+fn startswith_alpha(string: &str) -> bool {
+    string.chars().next().unwrap_or(' ').is_ascii_alphabetic()
+}
+
+fn checksum<'a>(ids: impl Iterator<Item = &'a str>) -> i32 {
+    let mut twice_total = 0;
+    let mut thrice_total = 0;
+    for id in ids {
+        let (twice, thrice) = has_repeats(id);
+        if twice {
+            twice_total += 1;
+        }
+        if thrice {
+            thrice_total += 1;
+        }
+    }
+
+    twice_total * thrice_total
+}
+
+fn has_repeats(id: &str) -> (bool, bool) {
+    let counts = count_letters(id);
+    let mut twice = false;
+    let mut thrice = false;
+    for count in counts.values() {
+        if *count == 2 {
+            twice = true;
+        }
+        if *count == 3 {
+            thrice = true;
+        }
+        if twice && thrice {
+            break;
+        }
+    }
+
+    (twice, thrice)
+}
+
+fn count_letters(id: &str) -> HashMap<char, i32> {
+    let mut map = HashMap::new();
+    for ch in id.chars() {
+        *map.entry(ch).or_insert(0) += 1;
+    }
+    map
+}
+
+fn common_id(ids: &[String]) -> Option<String> {
+    for id in ids.iter() {
+        for other in ids.iter() {
+            let common = similar_ids(id, other);
+            if common.is_some() {
+                return common;
+            }
+        }
+    }
+
+    None
+}
+
+fn similar_ids(id: &str, other: &str) -> Option<String> {
+    let mut difference: Option<_> = None;
+
+    for ((i, a), b) in id.char_indices().zip(other.chars()) {
+        if a != b {
+            if difference.is_none() {
+                difference = Some(i);
+            } else {
+                return None;
+            }
+        }
+    }
+
+    difference.map(|i| (&id[..i]).to_owned() + &id[(i + 1)..])
 }
 
 #[cfg(test)]
@@ -24,19 +96,54 @@ mod tests {
     #[test]
     fn test_load_ids() {
         assert_eq!(
-            load_ids(string_reader(
+            load_ids(BufReader::new(
                 "abcdef
 asdsadsad
 asddsfghesrgsg
 sdfsdfsdf
-    ",
+"
+                .as_bytes()
             ))
             .collect::<Vec<_>>(),
             vec!["abcdef", "asdsadsad", "asddsfghesrgsg", "sdfsdfsdf"]
         )
     }
 
-    fn string_reader(string: &'static str) -> impl BufRead {
-        BufReader::new(string.as_bytes())
+    #[test]
+    fn test_checksum() {
+        let input = vec![
+            "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",
+        ];
+
+        assert_eq!(checksum(input.into_iter()), 12);
+    }
+
+    #[test]
+    fn test_common_id() {
+        let input: Vec<_> = vec![
+            "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz",
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+        assert_eq!(common_id(&input), Some("fgij".to_owned()));
+    }
+
+    #[test]
+    fn test_similar_ids() {
+        assert_eq!(similar_ids("abc", "def"), None);
+        assert_eq!(similar_ids("abc", "abc"), None);
+        assert_eq!(similar_ids("abc", "ade"), None);
+        assert_eq!(similar_ids("abc", "abb"), Some("ab".to_owned()));
+        assert_eq!(similar_ids("abcdef", "abcjef"), Some("abcef".to_owned()));
+    }
+
+    #[test]
+    fn test_count_letters() {
+        assert_eq!(
+            count_letters("bababc"),
+            [('a', 2), ('b', 3), ('c', 1)].iter().cloned().collect()
+        )
     }
 }
